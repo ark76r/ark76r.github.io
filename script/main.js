a4814190451970352c40b4d8888c3b5f6d52683b
Arek <arychlinski@gmail.com>
2019-01-23 22:58:42 +0100

    rejestracja zdarzen online offline load ze skryptu (a nie z body)

--- script/main.js
+++ script/main.js
@@ -1,3 +1,9 @@
 function updateIndicator() {
      document.getElementById('online').textContent = navigator.onLine ? 'online' : 'offline';
 }
+
+
+window.addEventListener('load', updateIndicator);
+window.addEventListener('online', updateIndicator);
+window.addEventListener('offline', updateIndicator);
+
